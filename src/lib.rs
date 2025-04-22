eb43fbc27ce788f639f2dc6a4264e313d186ba0c
YopoTake3 <joelsi@kth.se>
2025-04-22 16:44:04 +0200

    Add descriptions for Deck functions

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub struct Hand {
 pub struct Deck {
     pub cards: Vec<Card>,
 }
+
 pub type Board = Vec<Card>;
 
 pub struct Player {
@@ -140,11 +141,42 @@ impl Deck {
         Deck{cards: deck}
     }
 
+    /// Shuffles deck randomly.
+    /// 
+    /// Behavior:
+    /// - Takes the cards currently in the deck and puts them in a random order.
+    /// - Will NOT reinitialize the deck to 52 cards. This can be done with the Deck.reset() function.
     pub fn shuffle(&mut self) {
         let mut rng = rand::rng();
         self.cards.shuffle(&mut rng);
     }
 
+    /// Draws the requested amount of cards and returns them as a Vector.
+    /// 
+    /// # Parameters
+    /// - 'amount': usize that dictates how many cards will be drawn
+    /// 
+    /// # Returns
+    /// - A Result<Vec<Card>, &´static str> where:
+    ///     - Vec<Card>: is a vector containing the cards that were drawn from the deck.
+    ///     - &´static string: is the error in case there are less cards in the deck than what was requested to draw.
+    /// 
+    /// Example:
+    /// '''
+    /// let mut deck = Deck::new();
+    ///
+    /// match deck.draw(5) {
+    ///     Ok(drawn_cards) => {
+    ///         println!("You drew the following cards:");
+    ///         for card in drawn_cards {
+    ///             println!("{:?}", card);
+    ///         }
+    ///     }
+    ///     Err(err) => {
+    ///         println!("Error drawing cards: {}", err);
+    ///     }
+    /// }
+    /// '''
     pub fn draw(&mut self, amount: usize) -> Result<Vec<Card>, &'static str> {
         if self.cards.len() < amount {
             return Err("Not enough cards in deck");
@@ -155,6 +187,7 @@ impl Deck {
         Ok(drawn_cards)
     }
 
+    /// Resets the deck, making it sorted (like buying a new playing card deck).
     pub fn reset(&mut self) {
         let mut deck: Vec<Card> = Vec::new();
         for suit in Suit::iter() {
